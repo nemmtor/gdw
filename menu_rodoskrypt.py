4a6e2686db8557332911aceac8f5ac38e8e2bc03
nemmtor <kacper0witas@gmail.com>
2019-08-20 15:26:24 +0200

    Poprawki stylistyczne cd. Zmieniam version na 1.0.7.1

--- menu_rodoskrypt.py
+++ menu_rodoskrypt.py
@@ -27,7 +27,7 @@ class Rodo_Skrypt(Window):
                               font=font10, fg='green')
         # Entry
         # Mail klienta
-        mail_label = tk.Label(mail_frame, text='Mail klienta', font=font10)
+        mail_label = tk.Label(mail_frame, text='Mail klienta:', font=font10)
         mail_entry = tk.Entry(mail_frame, width=entry_width, borderwidth=brwid, font=font9)
         mail_entry.bind_class(
             "Entry", "<Button-3><ButtonRelease-3>", self.show_menu)
